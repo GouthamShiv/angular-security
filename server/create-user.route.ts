5283868f0a865c2ec685996ba7db9912648d47ac
Goutham S <gouthamshine@gmail.com>
2022-04-16 19:41:14 +0530

    create user with sessionId

--- server/create-user.route.ts
+++ server/create-user.route.ts
@@ -3,6 +3,7 @@ import { db } from "./database";
 import { USERS } from "./database-data";
 import * as argon2 from "argon2";
 import { validatePassword } from "./password-validator";
+import { randomBytes } from "./security.utils";
 
 export function createUser(req: Request, res: Response) {
   const credentials = req.body;
@@ -12,12 +13,19 @@ export function createUser(req: Request, res: Response) {
   if (errors.length > 0) {
     res.status(400).json({ errors });
   } else {
-    argon2.hash(credentials.password).then((passwordDigest) => {
-      const user = db.createUser(credentials.email, passwordDigest);
-
-      console.log(USERS);
-
-      res.status(200).json({ id: user.id, email: user.email });
-    });
+    createUserWithSession(res, credentials);
   }
+}
+
+async function createUserWithSession(res: Response, credentials) {
+  const passwordDigest = await argon2.hash(credentials.password);
+  const user = db.createUser(credentials.email, passwordDigest);
+  const sessionId = await randomBytes(32).then((bytes) =>
+    bytes.toString("hex")
+  );
+
+  console.log(USERS);
+  console.log(sessionId);
+
+  res.status(200).json({ id: user.id, email: user.email });
 }
