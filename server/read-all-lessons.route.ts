5c908162bcd5fafac22bd29342946db7f6a5963a
Goutham S <gouthamshine@gmail.com>
2022-04-20 14:41:12 +0530

    lessions for logged in users only

--- server/read-all-lessons.route.ts
+++ server/read-all-lessons.route.ts
@@ -1,11 +1,15 @@
+import { db } from "./database";
+import { sessionStore } from "./session-store";
+import { Request, Response } from "express";
 
+export function readAllLessons(req: Request, res: Response) {
+  const sessionId = req.cookies["SESSIONID"];
 
-import {db} from "./database";
+  const isSessionValid = sessionStore.isSessionValid(sessionId);
 
-
-
-export function readAllLessons(req, res) {
-
-
-    return res.status(200).json(db.readAllLessons());
+  if (!isSessionValid) {
+    res.sendStatus(403);
+  } else {
+    return res.status(200).json({ lessons: db.readAllLessons() });
+  }
 }
