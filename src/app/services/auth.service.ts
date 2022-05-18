c480a6d7a3b64285eda7c68631c303b519ff52f2
goutham <gouthamshine@gmail.com>
2022-05-18 18:25:35 +0530

    auth0 impl

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,66 +1,108 @@
-
-import {filter} from 'rxjs/operators';
-import {Injectable} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {Observable, BehaviorSubject} from "rxjs";
-import {User} from "../model/user";
-import * as auth0 from 'auth0-js';
-import {Router} from "@angular/router";
+import { filter, shareReplay, tap } from "rxjs/operators";
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable, BehaviorSubject } from "rxjs";
+import { User } from "../model/user";
+import * as auth0 from "auth0-js";
+import { Router } from "@angular/router";
+import * as moment from "moment";
 
 export const ANONYMOUS_USER: User = {
-    id: undefined,
-    email: ''
+  id: undefined,
+  email: "",
 };
 
 const AUTH_CONFIG = {
-    clientID: 'hHhF4PWGY7vxLQH2HatJaUOertB1dDrU',
-    domain: "angularsecuritycourse.auth0.com"
+  clientID: "17PRmLy7GATrHxJbX50aDq4J7pV7RejT",
+  domain: "webstash.us.auth0.com",
 };
-
 
 @Injectable()
 export class AuthService {
+  auth0 = new auth0.WebAuth({
+    clientID: AUTH_CONFIG.clientID,
+    domain: AUTH_CONFIG.domain,
+    responseType: "token id_token",
+    redirectUri: "https://localhost:4200/lessons",
+    scope: "openid email",
+  });
 
-    auth0 = new auth0.WebAuth({
-        clientID: AUTH_CONFIG.clientID,
-        domain: AUTH_CONFIG.domain,
-        responseType: 'token id_token',
-        redirectUri: 'https://localhost:4200/lessons'
+  subject = new BehaviorSubject<User>(undefined);
+
+  user$: Observable<User> = this.subject
+    .asObservable()
+    .pipe(filter((user) => !!undefined));
+
+  constructor(private http: HttpClient, private router: Router) {
+    if (this.isLoggedIn()) {
+      this.userInfo();
+    }
+  }
+
+  login() {
+    this.auth0.authorize();
+  }
+
+  signUp() {
+    this.auth0.authorize({
+      mode: "signUp",
     });
+  }
 
-    private userSubject = new BehaviorSubject<User>(undefined);
+  logout() {
+    localStorage.removeItem("id_token");
+    localStorage.removeItem("expires_at");
+    this.router.navigate(["/lessons"]);
+  }
 
-    user$: Observable<User> = this.userSubject.asObservable().pipe(filter(user => !!user));
+  public isLoggedIn() {
+    return moment().isBefore(this.getExpiration());
+  }
 
-    constructor(private http: HttpClient, private router: Router) {
+  isLoggedOut() {
+    return !this.isLoggedIn();
+  }
 
+  private getExpiration() {
+    const expiration = localStorage.getItem("expires_at");
+    const expiresAt = JSON.parse(expiration);
+    return moment(expiresAt);
+  }
+
+  userInfo() {
+    const _self = this;
+    _self.http
+      .put<User>("/api/userinfo", null)
+      .pipe(
+        shareReplay(),
+        tap((user) => _self.subject.next(user))
+      )
+      .subscribe();
+  }
+
+  retrieveAuthInfo(): void {
+    const _self = this;
+    if (window.location.hash) {
+      _self.auth0.parseHash(
+        { hash: window.location.hash },
+        function (err, authResult) {
+          if (err) {
+            console.log("Could not parse the hash", err.message);
+            return;
+          } else if (authResult && authResult.idToken) {
+            window.location.hash = "";
+            console.log("Authentication successful, authResult: ", authResult);
+            _self.setSession(authResult);
+            _self.userInfo();
+          }
+        }
+      );
     }
+  }
 
-    login() {
-
-    }
-
-    signUp() {
-
-    }
-
-    logout() {
-
-    }
-
-    public isLoggedIn() {
-        return false;
-    }
-
-    isLoggedOut() {
-        return !this.isLoggedIn();
-    }
-
+  setSession(authResult: any) {
+    const expiresAt = moment().add(authResult.expiresIn, "seconds");
+    localStorage.setItem("id_token", authResult.idToken);
+    localStorage.setItem("expires_at", JSON.stringify(expiresAt.valueOf()));
+  }
 }
-
-
-
-
-
-
-
